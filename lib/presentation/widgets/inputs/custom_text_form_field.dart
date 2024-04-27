40c6a124a498a1f06187c805723fb3692800e653
Davichobits <davidruizdeveloper@gmail.com>
2024-04-27 14:41:08 -0500

    CustomFieldForm created

---
+++ lib/presentation/widgets/inputs/custom_text_form_field.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  const CustomTextFormField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    final border = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10),
+      borderSide: BorderSide(color: colors.primary)
+    );
+
+    return TextFormField(
+      onChanged: (value) {
+        // print(value);
+      },
+      validator: (value) {
+        if (value == null) return 'Campo es requerido';
+        if (value.isEmpty) return 'Campo es requerido';
+        if (value.trim().isEmpty) return 'Campo es requerido';
+        return null;
+      },
+      decoration: InputDecoration(
+        enabledBorder: border,
+        focusedBorder: border.copyWith(
+          borderSide: BorderSide(color: colors.primary)
+        )
+      ),
+    );
+  }
+}
